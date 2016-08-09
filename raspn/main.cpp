ac29ae0e574536f58c0537f259389f347f01c4bf
Marius Chirita <mariuselz@gmail.com>
2016-08-09 19:30:29 +0300

    Initial commit

---
+++ raspn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raspn
+//
+//  Created by Marius Chirita on 8/9/16.
+//  Copyright © 2016 NSHIFTED. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
