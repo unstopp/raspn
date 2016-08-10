5c918230de99072ef39835a3da04274ecc84817e
Marius Chirita <mariuselz@gmail.com>
2016-08-10 20:46:52 +0300

    Fixed the library integration in Xcode

--- raspn/main.cpp
+++ raspn/main.cpp
@@ -11,8 +11,8 @@
 #include <iostream>
 #include <stdint.h>
 
-#include "metawear/platform/btle_connection.h"
-#include "metawear/core/metawearboard.h"
+#include "btle_connection.h"
+#include "metawearboard.h"
 
 static void write_gatt_char(const void* caller, const MblMwGattChar *characteristic,
                             const uint8_t *value, uint8_t length) {
@@ -28,7 +28,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     
-    MblMwBtleConnection btle_conn = { write_gatt_char, read_gatt_char };
+    const MblMwBtleConnection btle_conn = { write_gatt_char, read_gatt_char };
     MblMwMetaWearBoard* board = mbl_mw_metawearboard_create(&btle_conn);
 
     
